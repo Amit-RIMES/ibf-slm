ecffa983f7d3438352997e3f6a86fa292051fb52
Amit <amit@rimes.int>
2026-06-17 13:41:22 +0700

    Fix migration down_revision: e0f1a2b3c4d5 (was placeholder f8b4d2e9a3c1)

--- alembic/versions/g1h2i3j4k5l6_add_api_key_call_count.py
+++ alembic/versions/g1h2i3j4k5l6_add_api_key_call_count.py
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "g1h2i3j4k5l6"
-down_revision: Union[str, None] = "f8b4d2e9a3c1"
+down_revision: Union[str, None] = "e0f1a2b3c4d5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
